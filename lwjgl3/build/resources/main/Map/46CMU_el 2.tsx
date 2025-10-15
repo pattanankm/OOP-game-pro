e3281bfe9c33cfb8ec7b821a65d6b7144c46dc26
pattanankm <khaimookbb212@gmail.com>
2025-10-16 06:56:37 +0700

    hahhaha

---
+++ lwjgl3/build/resources/main/Map/46CMU_el 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="CMU_el" tilewidth="46" tileheight="46" tilecount="475" columns="25">
+ <image source="CMU_el.png" width="1152" height="896"/>
+</tileset>
---
+++ lwjgl3/build/resources/main/Map/tree 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree" tilewidth="56" tileheight="56" tilecount="442" columns="34">
+ <image source="tree.png" width="1904" height="774"/>
+</tileset>
